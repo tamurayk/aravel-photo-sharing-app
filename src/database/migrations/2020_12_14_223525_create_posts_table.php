2ad9592c4f9cc331bc2fe595bea281d1bcb78fd7
田村行生 <>
2020-12-15 08:03:15 +0900

    add posts table

---
+++ src/database/migrations/2020_12_14_223525_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id')->nullable(false);
+            $table->string('image', 255);
+            $table->text('caption');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
